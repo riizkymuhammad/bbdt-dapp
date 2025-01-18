c646313a0db3eee9e641ea532db590bc0cf2258d
Muhammad Rizky <mrizky98@gmail.com>
2025-01-18 23:21:00 +0700

    add jwt login

--- bbdt-be/models/User.js
+++ bbdt-be/models/User.js
@@ -6,6 +6,8 @@ const UserSchema = new mongoose.Schema({
     role: { type: String, enum: ['donor', 'trustee', 'needy'], required: true },
     walletAddress: { type: String },
     status: { type: String, enum: ['approved', 'pending'], default: 'pending' },
+    password: { type: String, required: true },
+
 });
 
 module.exports = mongoose.model('User', UserSchema);
